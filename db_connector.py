5041a7701395c238903c3b76fe53dbd52827717c
pelegov <nave.peleg@gmail.com>
2022-07-15 21:55:14 +0300

    DB commit

--- db_connector.py
+++ db_connector.py
@@ -2,13 +2,13 @@ import pymysql
 import json
 
 # Establish connection with DB
-conn = pymysql.connect(host='remotemysql.com', port=3306, user='tZsOURTiTL', passwd='kjgwPi1fPd', db='tZsOURTiTL')
+conn = pymysql.connect(host='sql11.freemysqlhosting.net', port=3306, user='sql11506470', passwd='QwjD6b64t9', db='sql11506470')
 conn.autocommit(True)
 cursor = conn.cursor()
 
 ### SELECT for GET methods (get data)
 def get_value(user_id):
-    cursor.execute("SELECT * FROM tZsOURTiTL.users WHERE (user_id) = " + user_id + ";")
+    cursor.execute("SELECT * FROM sql11506470.users WHERE (user_id) = " + user_id + ";")
     user_name = ""
     for row in cursor:
         print(row)
@@ -19,21 +19,21 @@ def get_value(user_id):
 
 ### Insert for POST methods
 def post_value(id,name,creation):
-    cursor.execute("INSERT into tZsOURTiTL.users (user_id, user_name, creation_date) VALUES ({}, '{}','{}')".format(id, name, creation))
+    cursor.execute("INSERT into sql11506470.users (user_id, user_name, creation_date) VALUES ({}, '{}','{}')".format(id, name, creation))
     cursor.close()
     conn.close()
     return
 
 ### update for PUT methods
 def put_value():
-    cursor.execute("UPDATE tZsOURTiTL.users SET user_name = 'naveupdated' WHERE user_id = '1'")
+    cursor.execute("UPDATE sql11506470.users SET user_name = 'naveupdated' WHERE user_id = '1'")
     cursor.close()
     conn.close()
     return
 
 ### DELETE for DELETE methods
 def delete_value(id):
-    cursor.execute("DELETE FROM tZsOURTiTL.users WHERE (user_id) = ({})".format(id))
+    cursor.execute("DELETE FROM sql11506470.users WHERE (user_id) = ({})".format(id))
     cursor.close()
     conn.close()
     return
